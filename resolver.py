105867524f378415a74310f54b022936aab2de69
luce0207 <luce31.rml@gmail.com>
2024-05-10 18:36:21 +0200

    algo improvement for invalid grid

--- resolver.py
+++ resolver.py
@@ -9,41 +9,48 @@ def solve_sudoku(grille: list):
         return None
 
 
-def absentSurLigne(chiffre, grille, ligne):
+def occurrencesSurLigne(chiffre, grille, ligne):
+    nb_occ = 0
     for j in grille[ligne]:
         if j == chiffre:
-            return False
-    return True
+            nb_occ += 1
+    return nb_occ
 
 
-def absentSurColonne(chiffre, grille, colonne):
+def occurrencesSurColonne(chiffre, grille, colonne):
+    nb_occ = 0
     for i in range(9):
         if grille[i][colonne] == chiffre:
-            return False
-    return True
+            nb_occ += 1
+    return nb_occ
 
 
-def absentSurBloc(chiffre, grille, ligne, colonne):
+def occurrencesSurBloc(chiffre, grille, ligne, colonne):
+    nb_occ = 0
     ligneBloc = ligne - (ligne % 3)
     colonneBloc = colonne - (colonne % 3)
     for i in range(ligneBloc, ligneBloc + 3):
         for j in range(colonneBloc, colonneBloc + 3):
             if grille[i][j] == chiffre:
-                return False
-    return True
+                nb_occ += 1
+    return nb_occ
 
 
-def estValide(grille, position):
+def estValide(grille, position) -> bool:
     if position == 81:
         return True
     ligne = position // 9
     colonne = position % 9
-    if grille[ligne][colonne] != 0:
-        return estValide(grille, position + 1)
+    if grille[ligne][colonne] != 0:  # check Validity of this element
+        chiffre = grille[ligne][colonne]
+        return estValide(grille, position + 1) \
+            and occurrencesSurLigne(chiffre, grille, ligne) == 1 \
+            and occurrencesSurColonne(chiffre, grille, colonne) == 1 \
+            and occurrencesSurBloc(chiffre, grille, ligne, colonne) == 1
     for chiffre in range(1, 10):
-        if (absentSurLigne(chiffre, grille, ligne)
-                and absentSurColonne(chiffre, grille, colonne)
-                and absentSurBloc(chiffre, grille, ligne, colonne)):
+        if occurrencesSurLigne(chiffre, grille, ligne) == 0 \
+                and occurrencesSurColonne(chiffre, grille, colonne) == 0 \
+                and occurrencesSurBloc(chiffre, grille, ligne, colonne) == 0:
             grille[ligne][colonne] = chiffre
             if estValide(grille, position + 1):
                 return True
--- tests.py
+++ tests.py
@@ -35,19 +35,15 @@ class TestSudokuMethods(unittest.TestCase):
         self.assert_absentSurBloc(True, 6, 4, 3)
         self.assert_absentSurBloc(False, 9, 5, 7)
 
-    def test_estValide(self):
-        #self.assert_estValide(True, 0)
-        #self.assert_estValide(True, 0, grille=grille_test)
-        self.assert_estValide(False, 0, grille=[
-            [6, 6, 6, 2, 3, 4, 9, 7, 5],
-            [3, 9, 2, 5, 7, 6, 4, 1, 8],
-            [7, 4, 5, 8, 1, 9, 6, 3, 2],
-            [2, 3, 9, 7, 4, 5, 8, 6, 1],
-            [8, 6, 1, 9, 2, 3, 7, 5, 4],
-            [5, 7, 4, 6, 8, 1, 3, 2, 9],
-            [4, 8, 6, 3, 5, 2, 1, 9, 7],
-            [9, 5, 7, 1, 6, 8, 2, 4, 3],
-            [1, 2, 3, 4, 9, 7, 5, 8, 6]])
+    def test_grille_valide(self):
+        self.assert_estValide(True, 0, grille=copy.deepcopy(grille_solution))
+        # TODO assert computed grid equals tested one as tested was complete (should not modify input)
+
+    def test_grille_incomplete_and_completable(self):
+        self.assert_estValide(True, 0, grille=copy.deepcopy(grille_test))
+        # TODO assert computed grid with grille_solution
+
+    def test_grille_incomplete_and_not_completable(self):
         self.assert_estValide(False, 0, grille=[
             [0, 0, 0, 2, 3, 4, 9, 7, 5],
             [3, 9, 2, 5, 7, 6, 4, 1, 8],
@@ -59,6 +55,45 @@ class TestSudokuMethods(unittest.TestCase):
             [9, 5, 7, 1, 6, 8, 2, 4, 3],
             [1, 2, 3, 4, 9, 7, 5, 8, 6]])
 
+    def test_complete_and_not_valid(self):
+        self.assert_estValide(False, 0, grille=[
+            [6, 6, 6, 2, 3, 4, 9, 7, 5],
+            [3, 9, 2, 5, 7, 6, 4, 1, 8],
+            [7, 4, 5, 8, 1, 9, 6, 3, 2],
+            [2, 3, 9, 7, 4, 5, 8, 6, 1],
+            [8, 6, 1, 9, 2, 3, 7, 5, 4],
+            [5, 7, 4, 6, 8, 1, 3, 2, 9],
+            [4, 8, 6, 3, 5, 2, 1, 9, 7],
+            [9, 5, 7, 1, 6, 8, 2, 4, 3],
+            [1, 2, 3, 4, 9, 7, 5, 8, 6]])
+
+    def test_from_empty(self):
+        self.assert_estValide(True, 0, grille=[
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        # TODO assert computed grid with an expected result
+
+    @unittest.skip(reason="too long : need to improve algo")
+    def test_extreme(self):
+        self.assert_estValide(True, 0, grille=[
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0]])
+
+
     # -----------------
 
 
@@ -66,13 +101,13 @@ class TestSudokuMethods(unittest.TestCase):
         self.assertEqual(expected, estValide(copy.deepcopy(grille), position))
 
     def assert_absentSurLigne(self, expected, chiffre, ligne, grille=grille_test):
-        self.assertEqual(expected, absentSurLigne(chiffre, grille, ligne))
+        self.assertEqual(expected, occurrencesSurLigne(chiffre, grille, ligne) == 0)
 
     def assert_absentSurColonne(self, expected, chiffre, colonne, grille=grille_test):
-        self.assertEqual(expected, absentSurColonne(chiffre, grille, colonne))
+        self.assertEqual(expected, occurrencesSurColonne(chiffre, grille, colonne) == 0)
 
     def assert_absentSurBloc(self, expected, chiffre, ligne, colonne, grille=grille_test):
-        self.assertEqual(expected, absentSurBloc(chiffre, grille, ligne, colonne))
+        self.assertEqual(expected, occurrencesSurBloc(chiffre, grille, ligne, colonne) == 0)
 
 
 
